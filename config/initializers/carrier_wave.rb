e683cfe8129345182dd6f066f9638bd6c92a143d
Mohammad Ghorab <mody.moustafa@yahoo.com>
2019-07-17 11:12:24 +0200

    modified carrier_wave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,10 +2,27 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
         :provider => 'AWS',
-        :aws_access_key_id => ENV['s3_ACCESS_KEY'],
-        :aws_secret_access_key => ENV['s3_SECRET_KEY'],
+        :aws_access_key_id => ENV['S3_ACCESS_KEY'],
+        :aws_secret_access_key => ENV['S3_SECRET_KEY'],
         :region => 'us-east-2'
     }
-    config.fog_directory = ENV['s3_BUCKET']
+    config.fog_directory = ENV['S3_BUCKET']
   end
 end
+
+# CarrierWave.configure do |config|
+#   if Rails.env.production?
+#     config.fog_provider = 'fog/aws'
+#     config.fog_credentials = {
+#         :provider => 'AWS',
+#         :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+#         :aws_secret_access_key => ENV[‘AWS_SECRET_ACCESS_KEY’],
+#         :region => ‘ap-northeast-1’ # Tokyo
+#     }
+#     config.fog_directory = ENV[‘S3_BUCKET_NAME’]
+#     config.storage = :fog
+#   else
+#     config.storage = :file
+#     config.enable_processing = Rails.env.development?
+#   end
+# end
