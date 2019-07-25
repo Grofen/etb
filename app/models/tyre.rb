a2d677b1fd1f1828c9efc1f08df7d524dd958736
Mohammad Ghorab <mody.moustafa@yahoo.com>
2019-07-25 15:53:48 +0200

    Rearrange tyre model's validations

--- app/models/tyre.rb
+++ app/models/tyre.rb
@@ -5,16 +5,5 @@ class Tyre < ApplicationRecord
   friendly_id :name, use: :slugged
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
-  validates :width, presence: true
-  validates :ratio, presence: true
-  validates :rim, presence: true
-  validates :model, presence: true
-  validates :load, presence: true
-  validates :speed, presence: true
-  validates :season, presence: true
-  validates :brand_id, presence: true
-  validates :price, presence: true
-  validates :made_in, presence: true
-  validates :year, presence: true
-  validates :description, presence: true
+  validates :width, :ratio, :rim, :model, :load, :speed, :season, :brand_id, :price, :made_in, :year, :description, presence: true
 end
